bafcfb778e6402129d7f8102acfae29d140e8df3
scyclow <spike716@gmail.com>
2021-05-02 20:31:15 -0400

    add getData script, click through to artblocks

---
+++ getData.js
@@ -0,0 +1,16 @@
+const fetch = require('node-fetch')
+
+const tokens = 127
+const start = 44000000
+const end = start + tokens
+
+let promises = []
+for (let i = start; i < end; i++) {
+  promises.push(
+    fetch(`https://api.artblocks.io/token/${i}`)
+      .then(r => r.json())
+      .then(r => r['token hash'])
+  )
+}
+
+Promise.all(promises).then(r => console.log(JSON.stringify(r)))
